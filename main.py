08b67c1885a0c24c5c17d8b66f94b9429794d0ff
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-09-14 20:54:13 -0300

    Changing circle color with index finger

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ cap.set(3,1280)
 cap.set(4,720)
 handDetector = HandDetector(detectionCon=0.8)
 
+circleColor = 100,255,100
+circleX, circleY, circleRad  = 500, 200, 100
+
 while True:
     status, img = cap.read()
     img = cv2.flip(img,1)
     img = handDetector.findHands(img)
     list, _ = handDetector.findPosition(img)
 
-    cv2.circle(img, (300,300), 100, (100,255,100),cv2.FILLED)
+    if list:
+        fingerPoint = list[8]
+        if (circleX - circleRad) < fingerPoint[0] < (circleX + circleRad) and \
+                (circleY - circleRad) < fingerPoint[1] < (circleY + circleRad):
+            circleColor = 0,0,255
+
+        else:
+            circleColor = 100, 255, 100
+
+
+    cv2.circle(img, (circleX,circleY), circleRad, circleColor,cv2.FILLED)
 
     cv2.imshow("Hand image", img)
     cv2.waitKey(1)
