0af0e00470db61f7fb3e44df688df62bb1715859
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-09-14 00:28:36 -0300

    flip image and figure insertion

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ handDetector = HandDetector(detectionCon=0.8)
 
 while True:
     status, img = cap.read()
+    img = cv2.flip(img,1)
     img = handDetector.findHands(img)
     list, _ = handDetector.findPosition(img)
+
+    cv2.circle(img, (300,300), 100, (100,255,100),cv2.FILLED)
 
     cv2.imshow("Hand image", img)
     cv2.waitKey(1)
