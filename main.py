a1ecca0ac1fd8afab5abf2beb5aeb0f3ccf3b407
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-09-14 21:23:59 -0300

    Moving the object using two fingers

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3, 1280)
+cap.set(4, 720)
 handDetector = HandDetector(detectionCon=0.8)
 
-circleColor = 100,255,100
-circleX, circleY, circleRad  = 500, 200, 100
+circleColor = 100, 255, 100
+circleX, circleY, circleRad = 500, 200, 100
 
 while True:
     status, img = cap.read()
-    img = cv2.flip(img,1)
-    img = handDetector.findHands(img)
-    list, _ = handDetector.findPosition(img)
+    img = cv2.flip(img, 1)
+    img = handDetector.findHands(img, draw=False)
+    list, _ = handDetector.findPosition(img, draw=False)
 
     if list:
-        fingerPoint = list[8]
-        if (circleX - circleRad) < fingerPoint[0] < (circleX + circleRad) and \
-                (circleY - circleRad) < fingerPoint[1] < (circleY + circleRad):
-            circleColor = 0,0,255
+        fingerDistance, _, _ = handDetector.findDistance(8, 12, img, draw=False)
+
+        if fingerDistance <32:
+            fingerPoint = list[8]
+            if (circleX - circleRad) < fingerPoint[0] < (circleX + circleRad) and \
+                    (circleY - circleRad) < fingerPoint[1] < (circleY + circleRad):
+                circleColor = 0, 0, 255
+                circleX, circleY = fingerPoint
 
         else:
             circleColor = 100, 255, 100
